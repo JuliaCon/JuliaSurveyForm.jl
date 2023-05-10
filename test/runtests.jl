17c3bce1d9056a72a1483205b5f1abe4e03ec4a4
Viral B. Shah <ViralBShah@users.noreply.github.com>
2023-05-10 14:54:24 -0400

    Generate all the markdown files for all languages

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,14 +19,16 @@ survey_ko_KR = JuliaSurveyForm.translate(survey_zh_CN, ko_KR)
 # reformat zh_CN
 # JuliaSurveyForm.emit_toml("JuliaSurvey.zh_CN.toml", survey_zh_CN)
 JuliaSurveyForm.emit_toml(surveys_dir("JuliaSurvey.en_US.toml"), survey_en_US)
+JuliaSurveyForm.emit_toml(surveys_dir("JuliaSurvey.es_ES.toml"), survey_es_ES)
 JuliaSurveyForm.emit_toml(surveys_dir("JuliaSurvey.zh_CN.toml"), survey_zh_CN)
 JuliaSurveyForm.emit_toml(surveys_dir("JuliaSurvey.zh_TW.toml"), survey_zh_TW)
 JuliaSurveyForm.emit_toml(surveys_dir("JuliaSurvey.ja_JP.toml"), survey_ja_JP)
 JuliaSurveyForm.emit_toml(surveys_dir("JuliaSurvey.ko_KR.toml"), survey_ko_KR)
-JuliaSurveyForm.emit_toml(surveys_dir("JuliaSurvey.es_ES.toml"), survey_es_ES)
 
 
 JuliaSurveyForm.emit_markdown("JuliaSurvey.en_US.md", survey_en_US)
+JuliaSurveyForm.emit_markdown("JuliaSurvey.es_ES.md", survey_es_ES)
 JuliaSurveyForm.emit_markdown("JuliaSurvey.zh_CN.md", survey_zh_CN)
 JuliaSurveyForm.emit_markdown("JuliaSurvey.zh_TW.md", survey_zh_TW)
-JuliaSurveyForm.emit_markdown("JuliaSurvey.es_ES.md", survey_es_ES)
+JuliaSurveyForm.emit_markdown("JuliaSurvey.ja_JP.md", survey_ja_JP)
+JuliaSurveyForm.emit_markdown("JuliaSurvey.ko_KR.md", survey_ko_KR)
